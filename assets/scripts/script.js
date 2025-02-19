ac420d1269260f2597532435d05778a3de3cdd26
Giulliano <tharsoweb@gmail.com>
2025-02-19 15:27:37 -0300

    botão scroll up

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,8 @@ const menuIcon = document.querySelector(".j_menu_icon")
 const hamburgerIcon = "/assets/images/hamburger.svg"
 const closeIcon = "/assets/images/close.svg"
 
+const scrollUpButton = document.querySelector(".j_scroll_up")
+
 // FIXED HEADER
 const setFixedHeader = () => window.scrollY > 0
     ? header.classList.add("scrolling")
@@ -12,12 +14,22 @@ const setFixedHeader = () => window.scrollY > 0
 
 setFixedHeader()
 
-window.addEventListener("scroll", () => {
-    setFixedHeader()
-})
-
 // MOBILE MENU
 menuIcon.addEventListener("click", () => {
     menuNavigationContext.classList.toggle("open-menu")
     menuIcon.src = menuNavigationContext.classList.contains("open-menu") ? closeIcon : hamburgerIcon
+})
+
+// SROLL UP
+const showScrollUpButton = () => window.scrollY > 0
+? scrollUpButton.classList.add("visible")
+: scrollUpButton.classList.remove("visible")
+
+showScrollUpButton()
+
+scrollUpButton.addEventListener("click", () => window.scrollTo(0, 0))
+
+window.addEventListener("scroll", () => {
+    setFixedHeader()
+    showScrollUpButton()
 })
