fce27302e914df581565a5c5291436384e7b4a42
Giulliano <tharsoweb@gmail.com>
2025-02-19 13:49:39 -0300

    seção apresentação

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,22 @@
+const header = document.querySelector(".j_header")
+
 const menuNavigationContext = document.querySelector(".j_menu_navigation_context")
 const menuIcon = document.querySelector(".j_menu_icon")
 const hamburgerIcon = "/assets/images/hamburger.svg"
 const closeIcon = "/assets/images/close.svg"
 
+// FIXED HEADER
+const setFixedHeader = () => window.scrollY > 0
+    ? header.classList.add("scrolling")
+    : header.classList.remove("scrolling")
+
+setFixedHeader()
+
+window.addEventListener("scroll", () => {
+    setFixedHeader()
+})
+
+// MOBILE MENU
 menuIcon.addEventListener("click", () => {
     menuNavigationContext.classList.toggle("open-menu")
     menuIcon.src = menuNavigationContext.classList.contains("open-menu") ? closeIcon : hamburgerIcon
